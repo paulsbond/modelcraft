3d17a9a9380a41608dcf15a0423c048724044e1f
Paul Bond <git@paulsbond.co.uk>
2019-11-27 22:07:18 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+  name="buccaneer_pipeline2",
+  version="0.1.1",
+  description="Buccaneer model building pipeline",
+  author="Paul Bond",
+  author_email="paul.bond@york.ac.uk",
+  url="https://github.com/paulsbond/buccaneer_pipeline2",
+)
