03380ed758116cb0914ec0388237d1581d2eb44f
Paul Bond <git@paulsbond.co.uk>
2019-12-02 19:46:29 +0000

    Added a tests submodule

---
+++ autocoord/tests/test_gemmineer.py
@@ -0,0 +1,9 @@
+import autocoord.gemmineer as gemmineer
+
+
+def test_number_of_known():
+    assert len(gemmineer._known_protein_residues) == 22
+
+
+def test_unk_is_known():
+    assert "UNK" in gemmineer._known_protein_residues
