ed501bacc2f01356180701b4364a24e26ae25875
Paul Bond <git@paulsbond.co.uk>
2019-12-02 20:06:57 +0000

    Updated indentation

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
 with open("README.md", "r") as f:
-  long_description = f.read()
+    long_description = f.read()
 
 setuptools.setup(
-  name="autocoord",
-  version="0.0.1",
-  author="Paul Bond",
-  author_email="paul.bond@york.ac.uk",
-  description="Automated model building pipeline for X-ray crystallography and cryo-EM",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url="https://github.com/paulsbond/autocoord",
-  packages=setuptools.find_packages(),
-  classifiers=[
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-    "Operating System :: OS Independent",
-  ],
-  python_requires=">=3.6",
-  install_requires=["gemmi"],
+    name="autocoord",
+    version="0.0.1",
+    author="Paul Bond",
+    author_email="paul.bond@york.ac.uk",
+    description="Automated model building pipeline for X-ray crystallography and cryo-EM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paulsbond/autocoord",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["gemmi"],
 )
