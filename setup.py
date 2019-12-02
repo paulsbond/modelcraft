7cc8046c932dec654d03be29f6236e619a5dd620
Paul Bond <git@paulsbond.co.uk>
2019-12-02 17:30:07 +0000

    Updated package information

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 import setuptools
 
+with open("README.md", "r") as f:
+  long_description = f.read()
+
 setuptools.setup(
   name="autocoord",
   version="0.0.0",
-  description="Automated model building pipeline",
-  url="https://github.com/paulsbond/autocoord",
   author="Paul Bond",
   author_email="paul.bond@york.ac.uk",
-  license="LGPL-2.1",
-  install_requires=["gemmi"],
+  description="Automated model building pipeline",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/paulsbond/autocoord",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)"
+    "Operating System :: OS Independent",
+  ],
+  python_requires=">=3.6",
 )
