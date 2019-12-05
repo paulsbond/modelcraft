586c40fd03ab1b4a43c2b642373aa299564c29ff
Paul Bond <git@paulsbond.co.uk>
2019-12-05 16:58:13 +0000

    Added a command line script

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/paulsbond/modelcraft",
+    scripts=['bin/modelcraft'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
