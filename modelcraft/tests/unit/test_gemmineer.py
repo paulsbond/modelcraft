47aed57496810ed39da6a7f01c0776e1c25fd3c3
Paul Bond <git@paulsbond.co.uk>
2019-12-06 15:16:35 +0000

    Testing gemmineer model_stats exception

--- modelcraft/tests/unit/test_gemmineer.py
+++ modelcraft/tests/unit/test_gemmineer.py
@@ -1,3 +1,4 @@
+from modelcraft.tests import data_path
 import modelcraft.gemmineer as gemmineer
 
 
@@ -7,3 +8,8 @@ def test_number_of_known():
 
 def test_unk_is_known():
     assert "UNK" in gemmineer._known_protein_residues
+
+
+def test_1kv9_model_stats():
+    xyzin = data_path("1kv9_model.pdb")
+    gemmineer.model_stats(xyzin)
