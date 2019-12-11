b92ef1d4e9bc1a8502428bdeb42de8bf305452f7
Paul Bond <git@paulsbond.co.uk>
2019-12-11 14:29:19 +0000

    Checking for the CCP4 environment

--- modelcraft/__main__.py
+++ modelcraft/__main__.py
@@ -1,8 +1,11 @@
 from modelcraft.pipeline import Pipeline
+import os
 import sys
 
 
 def main(args=None):
+    if "CCP4" not in os.environ:
+        sys.exit("CCP4 environment not set")
     if args is None:
         args = sys.argv[1:]
     Pipeline(args)
