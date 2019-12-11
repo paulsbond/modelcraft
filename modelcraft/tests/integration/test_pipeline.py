9ba33541d588c71302034ef2b0bb464d2dee28e0
Paul Bond <git@paulsbond.co.uk>
2019-12-11 14:44:14 +0000

    Added a pipeline test

---
+++ modelcraft/tests/integration/test_pipeline.py
@@ -0,0 +1,19 @@
+from modelcraft.tests import data_path
+from modelcraft.__main__ import main
+import os
+import shutil
+import uuid
+
+
+def test_1kv9():
+    tmp_dir = "tmp%s" % uuid.uuid4()
+    os.mkdir(tmp_dir)
+    os.chdir(tmp_dir)
+    args = [
+        "--mtzin", data_path("1kv9_data.mtz"),
+        "--seqin", data_path("1kv9_sequence.fasta"),
+        "--cycles", "2"
+    ]
+    main(args)
+    os.chdir("..")
+    shutil.rmtree(tmp_dir)
