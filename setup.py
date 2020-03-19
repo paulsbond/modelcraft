dc4d5945c21484c66077ac4a6ad092a4bb76fcce
Paul Bond <git@paulsbond.co.uk>
2020-03-19 13:26:00 +0000

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="modelcraft",
-    version="0.1.4",
+    version="0.1.5",
     author="Paul Bond",
     author_email="paul.bond@york.ac.uk",
     description="Automated model building pipeline for X-ray crystallography",
