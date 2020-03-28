dd334a793375d925dce96ee78e82ad5dcd07c387
Paul Bond <git@paulsbond.co.uk>
2020-03-28 19:30:19 +0000

    Refactoring reflections (in progress)

--- modelcraft/reflections.py
+++ modelcraft/reflections.py
@@ -1,8 +1,9 @@
-import os
 import gemmi
+import itertools
+import os
 
 
-class ReflectionFile:
+class DataFile:
     def __init__(self, path):
         self.path = os.path.abspath(path)
         mtz = gemmi.read_mtz_file(path)
@@ -14,86 +15,47 @@ class ReflectionFile:
         self.resolution_low = mtz.resolution_low()
 
         self.columns = {col.label for col in mtz.columns}
-        self.fsigfs = columns_with_types(mtz, ["F", "Q"])
-        self.frees = columns_with_types(mtz, ["I"])
-        self.abcds = columns_with_types(mtz, ["A", "A", "A", "A"])
-        self.phifoms = set()
-        self.fphis = set()
-        for i in range(len(mtz.columns)):
-            col1 = mtz.columns[0]
-            if col1.type == "F" and i < len(mtz.columns) - 1:
-                col2 = mtz.columns[i + 1]
-                if col2.type == "Q":
-                    labels = ",".join((col1.label, col2.label))
-                    self.fsigfs.add(labels)
-                    i + 1
-            elif col1.type == "I":
-                self.frees.add(col1.label)
+        self.fsigfs = self._data_items(mtz, ["F", "Q"])
+        self.frees = self._data_items(mtz, ["I"])
+        self.abcds = self._data_items(mtz, ["A", "A", "A", "A"])
+        self.phifoms = self._data_items(mtz, ["P", "W"], sequential=False)
+        self.fphis = self._data_items(mtz, ["F", "P"])
 
-    def __contains__(self, column):
-        return column in self.columns
+    def __contains__(self, columns):
+        return all(col in self.columns for col in columns.split(","))
 
+    def _data_items(self, mtz, types, sequential=True):
+        items = set()
+        if sequential:
+            mtz_types = [col.type for col in mtz.columns]
+            i = 0
+            while i < len(mtz_types) - len(types) + 1:
+                if mtz_types[i : i + len(types)] == types:
+                    item = ",".join(col.label for col in mtz.columns[i : i + len(types)])
+                    items.add(item)
+                    i += 3
+                i += 1
+        else:
+            columns = [mtz.columns_with_type(t) for t in types]
+            for combination in itertools.product(*columns):
+                items.add(",".join(col.label for col in combination))
+        return items
 
-def columns_with_types(mtz, types):
-    pass
+    def _phifoms(self, mtz):
+        def is_phi(column):
+            if column.type != "P":
+                return False
+            label = column.label.lower()
+            return "ph" in label and "del" not in label
 
+        def is_fom(column):
+            if column.type != "W":
+                return False
+            label = column.label.lower()
+            return "fom" in label
 
-def fo_columns(mtz):
-    def differ_by_sig(label1, label2):
-        label1 = label1.lower().replace("sig", "")
-        label2 = label2.lower().replace("sig", "")
-        return label1 == label2
-
-    for f in mtz.columns_with_type("F"):
-        for sig in mtz.columns_with_type("Q"):
-            if differ_by_sig(f.label, sig.label):
-                yield "%s,%s" % (f.label, sig.label)
-
-
-def free_columns(mtz):
-    for column in mtz.columns_with_type("I"):
-        if "free" in column.label.lower():
-            yield column.label
-
-
-def hl_columns(mtz):
-    def belong_to_the_same_group(label1, label2):
-        if len(label1) != len(label2):
-            return False
-        differences = 0
-        for i in range(len(label1)):
-            if label1[i] != label2[i]:
-                differences += 1
-        return differences == 1
-
-    groups = []
-    for column in mtz.columns_with_type("A"):
-        grouped = False
-        for group in groups:
-            if belong_to_the_same_group(column.label, group[0]):
-                grouped = True
-                group.append(column.label)
-                if len(group) == 4:
-                    yield ",".join(sorted(group))
-        if not grouped:
-            groups.append([column.label])
-
-
-def phifom_columns(mtz):
-    def is_phi(column):
-        if column.type != "P":
-            return False
-        label = column.label.lower()
-        return "ph" in label and "del" not in label
-
-    def is_fom(column):
-        if column.type != "W":
-            return False
-        label = column.label.lower()
-        return "fom" in label
-
-    phis = [column for column in mtz.columns if is_phi(column)]
-    foms = [column for column in mtz.columns if is_fom(column)]
-    for phi in phis:
-        for fom in foms:
-            yield "%s,%s" % (phi.label, fom.label)
+        phis = [column for column in mtz.columns if is_phi(column)]
+        foms = [column for column in mtz.columns if is_fom(column)]
+        for phi in phis:
+            for fom in foms:
+                yield "%s,%s" % (phi.label, fom.label)
--- modelcraft/tests/unit/test_reflections.py
+++ modelcraft/tests/unit/test_reflections.py
@@ -1,7 +1,95 @@
 from modelcraft.tests import data_path
-from modelcraft.reflections import ReflectionFile
+from modelcraft.reflections import DataFile
 
 
-def test_resolution():
-    hklin = ReflectionFile(data_path("1kv9_data.mtz"))
-    assert hklin.resolution() == 1.80
+def test_1kv9():
+    hklin = DataFile(data_path("1kv9_data.mtz"))
+    assert round(hklin.cell.a, 3) == 54.807
+    assert round(hklin.cell.b, 3) == 57.437
+    assert round(hklin.cell.c, 3) == 67.523
+    assert round(hklin.cell.alpha, 2) == 89.65
+    assert round(hklin.cell.beta, 2) == 69.34
+    assert round(hklin.cell.gamma, 2) == 68.39
+    assert hklin.spacegroup.hm == "P 1"
+    assert hklin.num_reflections == 51422
+    assert round(hklin.resolution_high, 2) == 1.80
+    assert round(hklin.resolution_low, 2) == 29.87
+    columns = ["H", "K", "L", "FREE", "FP", "SIGFP", "HLA", "HLB", "HLC", "HLD"]
+    assert len(hklin.columns) == len(columns)
+    assert all(col in hklin for col in columns)
+    assert ",".join(columns) in hklin
+    assert "LABEL_NOT_IN_FILE" not in hklin
+    assert hklin.frees == {"FREE"}
+    assert hklin.fsigfs == {"FP,SIGFP"}
+    assert hklin.abcds == {"HLA,HLB,HLC,HLD"}
+    assert hklin.phifoms == set()
+    assert hklin.fphis == set()
+
+
+def test_hewl():
+    hklin = DataFile(data_path("hewl_data.mtz"))
+    assert hklin.spacegroup.hm == "P 43 21 2"
+    assert hklin.num_reflections == 10055
+    assert round(hklin.resolution_high, 2) == 1.86
+    assert round(hklin.resolution_low, 2) == 55.23
+    columns = [
+        "H",
+        "K",
+        "L",
+        "F_New",
+        "SIGF_New",
+        "DANO_New",
+        "SIGDANO_New",
+        "F_New(+)",
+        "SIGF_New(+)",
+        "F_New(-)",
+        "SIGF_New(-)",
+        "IMEAN_New",
+        "SIGIMEAN_New",
+        "I_New(+)",
+        "SIGI_New(+)",
+        "I_New(-)",
+        "SIGI_New(-)",
+        "ISYM_New",
+        "FreeR_flag",
+        "FWT",
+        "PHWT",
+        "PHIB",
+        "FOM",
+        "FPFOM",
+        "HLA",
+        "HLB",
+        "HLC",
+        "HLD",
+        "HLanomA",
+        "HLanomB",
+        "HLanomC",
+        "HLanomD",
+        "parrot.ABCD.A",
+        "parrot.ABCD.B",
+        "parrot.ABCD.C",
+        "parrot.ABCD.D",
+        "parrot.F_phi.F",
+        "parrot.F_phi.phi",
+    ]
+    assert len(hklin.columns) == len(columns)
+    assert all(col in hklin for col in columns)
+    assert hklin.frees == {"FreeR_flag"}
+    assert hklin.fsigfs == {"F_New,SIGF_New"}
+    assert hklin.abcds == {
+        "HLA,HLB,HLC,HLD",
+        "HLanomA,HLanomB,HLanomC,HLanomD",
+        "parrot.ABCD.A,parrot.ABCD.B,parrot.ABCD.C,parrot.ABCD.D",
+    }
+    assert hklin.phifoms == {
+        "PHWT,FPFOM",
+        "parrot.F_phi.phi,FPFOM",
+        "PHIB,FPFOM",
+        "PHIB,FOM",
+        "parrot.F_phi.phi,FOM",
+        "PHWT,FOM",
+    }
+    assert hklin.fphis == {
+        "FWT,PHWT",
+        "parrot.F_phi.F,parrot.F_phi.phi",
+    }
