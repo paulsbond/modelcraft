d16fe34f6978487d3b1551b6fe65cf86db095598
Paul Bond <git@paulsbond.co.uk>
2020-04-10 19:15:47 +0100

    GUI update

--- modelcraft/gui/__main__.py
+++ modelcraft/gui/__main__.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 from modelcraft.reflections import DataFile
 from PySide2.QtWidgets import (
     QApplication,
+    QComboBox,
     QDialog,
     QFileDialog,
     QGroupBox,
@@ -14,27 +17,54 @@ from PySide2.QtWidgets import (
 
 
 class ReflectionGroupBox(QGroupBox):
-    def __init__(self, parentDialog):
+    def __init__(self):
         super().__init__("Reflection Data")
-        self.parentDialog = parentDialog
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        hbox = QHBoxLayout()
         button = QPushButton("Select File")
         button.clicked.connect(self.select_file)
-        hbox.addWidget(button)
         self.file_label = QLabel("No file selected.")
+        hbox = QHBoxLayout()
+        hbox.addWidget(button)
         hbox.addWidget(self.file_label)
         layout.addLayout(hbox)
 
-        self.spacegroup_label = QLabel("Spacegroup: ")
+        self.cell_label = QLabel("Cell:")
+        self.spacegroup_label = QLabel("Spacegroup:")
+        self.volume_label = QLabel("ASU Volume:")
+        self.resolution_label = QLabel("Resolution Limits:")
+        self.reflection_label = QLabel("Number of Reflections:")
+        layout.addWidget(self.cell_label)
         layout.addWidget(self.spacegroup_label)
+        layout.addWidget(self.volume_label)
+        layout.addWidget(self.resolution_label)
+        layout.addWidget(self.reflection_label)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(QLabel("Observations"))
+        self.colin_fo_combo = QComboBox()
+        hbox.addWidget(self.colin_fo_combo)
+        layout.addLayout(hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(QLabel("Free-R Flag"))
+        self.colin_free_combo = QComboBox()
+        hbox.addWidget(self.colin_free_combo)
+        hbox.addWidget(QLabel("Set"))
+        self.free_flag_combo = QComboBox()
+        hbox.addWidget(self.free_flag_combo)
+        layout.addLayout(hbox)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(QLabel("Phases"))
+        self.phases_combo = QComboBox()
+        hbox.addWidget(self.phases_combo)
+        layout.addLayout(hbox)
 
     def select_file(self):
         path, _ = QFileDialog.getOpenFileName(
-            parent=self.parentDialog,
             caption="Select Reflection Data File",
             filter="MTZ Files [*.mtz] (*.mtz) ;; All Files [*.*] (*.*)",
             options=QFileDialog.Options(),
@@ -42,7 +72,33 @@ class ReflectionGroupBox(QGroupBox):
         if path != "":
             self.file_label.setText(os.path.basename(path))
             hklin = DataFile(path)
+            self.cell_label.setText(
+                "Cell: %.3f  %.3f  %.3f  %.2f  %.2f  %.2f"
+                % (hklin.cell.a, hklin.cell.b, hklin.cell.c, hklin.cell.alpha, hklin.cell.beta, hklin.cell.gamma)
+            )
+            print(dir(hklin.cell))
+            print(dir(hklin.spacegroup))
             self.spacegroup_label.setText("Spacegroup: " + hklin.spacegroup.hm)
+            self.volume_label.setText(u"ASU Volume: %.0f Å" % (hklin.cell.volume / len(hklin.spacegroup.operations())))
+            self.resolution_label.setText(
+                u"Resolution limits: %.2f – %.2f Å" % (hklin.resolution_high, hklin.resolution_low)
+            )
+            self.reflection_label.setText("Number of Reflections: %d" % hklin.num_reflections)
+
+
+class AsuContentsGroupBox(QGroupBox):
+    def __init__(self):
+        super().__init__("ASU Contents")
+
+
+class PhasesGroupBox(QGroupBox):
+    def __init__(self):
+        super().__init__("Phases")
+
+
+class OptionsGroupBox(QGroupBox):
+    def __init__(self):
+        super().__init__("Options")
 
 
 class Window(QDialog):
@@ -51,7 +107,10 @@ class Window(QDialog):
         self.setWindowTitle("ModelCraft")
         layout = QVBoxLayout()
         self.setLayout(layout)
-        layout.addWidget(ReflectionGroupBox(self))
+        layout.addWidget(ReflectionGroupBox())
+        layout.addWidget(AsuContentsGroupBox())
+        layout.addWidget(PhasesGroupBox())
+        layout.addWidget(OptionsGroupBox())
 
 
 def main():
