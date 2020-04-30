acdd33a68ecb1ad67c42cacf8d2b61b7c7a45cb4
Paul Bond <git@paulsbond.co.uk>
2020-04-30 20:09:41 +0100

    Updated sheetbend job

--- modelcraft/sheetbend.py
+++ modelcraft/sheetbend.py
@@ -1,29 +1,29 @@
 import gemmi
-from modelcraft.data import DataItem, write_mtz
 from modelcraft.job import Job
-from modelcraft.model import write_mmcif
+from modelcraft.reflections import FsigF, FreeRFlag, write_mtz
+from modelcraft.structure import write_mmcif
 
 
 class Sheetbend(Job):
-    def __init__(self, fsigf: DataItem, free: DataItem, structure: gemmi.Structure):
+    def __init__(self, fsigf: FsigF, freer: FreeRFlag, structure: gemmi.Structure):
         super().__init__()
         args = []
 
         hklin = self.path("hklin.mtz")
-        write_mtz(hklin, [fsigf, free])
         args += ["-mtzin", hklin]
         args += ["-colin-fo", fsigf.label()]
-        args += ["-colin-free", free.label()]
+        args += ["-colin-free", freer.label()]
+        write_mtz(hklin, [fsigf, freer])
 
         xyzin = self.path("xyzin.cif")
-        write_mmcif(xyzin, structure)
         args += ["-pdbin", xyzin]
-
-        xyzout = self.path("xyzout.cif")
-        args += ["-pdbout", xyzout]
+        write_mmcif(xyzin, structure)
 
         args += ["-cycles", "12"]
         args += ["-resolution-by-cycle", "6,6,3"]
+
+        xyzout = self.path("xyzout.cif")
+        args += ["-pdbout", xyzout]
 
         self.run("csheetbend", args)
         self.structure = gemmi.read_structure(xyzout)
--- modelcraft/tests/integration/test_sheetbend.py
+++ modelcraft/tests/integration/test_sheetbend.py
@@ -1,14 +1,14 @@
 import gemmi
-from modelcraft.data import DataItem
+from modelcraft.reflections import FsigF, FreeRFlag
 from modelcraft.sheetbend import Sheetbend
+from modelcraft.structure import model_stats
 from modelcraft.tests import data_path
-from modelcraft.model import model_stats
 
 
 def test_1kv9():
     mtz = gemmi.read_mtz_file(data_path("1kv9_data.mtz"))
-    fsigf = DataItem(mtz, ["FP", "SIGFP"])
-    free = DataItem(mtz, ["FREE"])
+    fsigf = FsigF(mtz, "FP,SIGFP")
+    freer = FreeRFlag(mtz, "FREE")
     structure = gemmi.read_structure(data_path("1kv9_model.pdb"))
-    sheetbend = Sheetbend(fsigf, free, structure)
+    sheetbend = Sheetbend(fsigf, freer, structure)
     assert model_stats(structure) == model_stats(sheetbend.structure)
