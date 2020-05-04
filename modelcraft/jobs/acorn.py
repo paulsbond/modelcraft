fa18769dfb1d4aefa7e3addfeb9c079efc1d4a93
Paul Bond <git@paulsbond.co.uk>
2020-05-05 00:02:16 +0100

    Fix to acorn placeholder

--- modelcraft/jobs/acorn.py
+++ modelcraft/jobs/acorn.py
@@ -1,9 +1,10 @@
-from modelcraft.job import Job
+from .job import Job
 
 
 class Acorn(Job):
-    def __init__(self, args, directory, hklin):
-        super().__init__(directory)
+    def __init__(self):
+        super().__init__()
+        self.finish()
 
 
 # /home/paul/Programs/ccp4-7.0/bin/mtzdump \
