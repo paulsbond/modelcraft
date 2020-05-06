5ef934e9f58b270229fd0a31b7b00b8aa9cc0b42
Paul Bond <git@paulsbond.co.uk>
2020-05-06 21:47:31 +0100

    Working Coot prune (with PDB format)

--- modelcraft/jobs/coot.py
+++ modelcraft/jobs/coot.py
@@ -11,23 +11,26 @@ class Coot(Job):
         self, structure: gemmi.Structure, fphi_best: FPhi, fphi_diff: FPhi, script: str
     ):
         super().__init__()
-        xyzin = self.path("xyzin.cif")
+        xyzin = self.path("xyzin.pdb")  # TODO: Change to CIF
         hklin = self.path("hklin.mtz")
         script_path = self.path("script.py")
-        xyzout = self.path("xyzout.cif")
+        xyzout = self.path("xyzout.pdb")  # TODO: Change to CIF
+        script = (
+            f"read_pdb('{xyzin}')\n"
+            f"make_and_draw_map('{hklin}', '{fphi_best.label(0)}', '{fphi_best.label(1)}', '', 0, 0)\n"
+            f"make_and_draw_map('{hklin}', '{fphi_diff.label(0)}', '{fphi_diff.label(1)}', '', 0, 1)\n"
+            f"{script}\n"
+            f"write_pdb_file(0, '{xyzout}')\n"  # TODO: write_cif_file
+            "exit()\n"
+        )
 
-        script += f"\nwrite_pdb_file(0, '{xyzout}')\n"
-        script += "exit()"
         with open(script_path, "w") as script_file:
             script_file.write(script)
-        write_mmcif(xyzin, structure)
+        structure.write_pdb(xyzin)  # TODO: Change to CIF
+        # write_mmcif(xyzin, structure)
         write_mtz(hklin, [fphi_best, fphi_diff])
 
         args = []
-        # xyzin is not being read (maybe due to cif)
-        # change both this and the map reading to be done with scripting commands)
-        args += [xyzin]
-        args += [hklin]
         args += ["--no-graphics"]
         args += ["--no-guano"]
         args += ["--no-state-script"]
