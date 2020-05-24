aee553ce68f44c337c8ef65c2b8f31a7c1be8475
Paul Bond <git@paulsbond.co.uk>
2020-05-24 13:58:55 +0100

    Writing out a command script for debugging

--- modelcraft/jobs/job.py
+++ modelcraft/jobs/job.py
@@ -18,6 +18,7 @@ class Job:
         self._directory = os.path.abspath("job_%s" % _generate_id())
         self._stdout = self.path("stdout.txt")
         self._stderr = self.path("stderr.txt")
+        self._cmd = self.path("cmd.sh")
         os.mkdir(self._directory)
         self.start_time = time.time()
         self.finish_time = None
@@ -34,6 +35,7 @@ class Job:
     ) -> None:
         if distutils.spawn.find_executable(executable) is None:
             raise ValueError("Executable '%s' not found" % executable)
+        self._write_cmd_script(executable, arguments, stdin)
         process = subprocess.Popen(
             args=[executable] if arguments is None else ([executable] + arguments),
             stdin=None if stdin is None else subprocess.PIPE,
@@ -45,7 +47,25 @@ class Job:
             for line in stdin:
                 process.stdin.write(line + "\n")
             process.stdin.close()
+
         process.wait()
+
+    def _write_cmd_script(
+        self, executable: str, arguments: List[str] = None, stdin: List[str] = None
+    ) -> None:
+        script = "#!/usr/bin/env bash\n\n"
+        script += executable
+        if arguments is not None:
+            script += " " + " ".join(arguments)
+        script += f" > {self._stdout} 2> {self._stderr}"
+        if stdin is not None:
+            script += " << EOF\n"
+            for line in stdin:
+                script += f"{line}\n"
+            script += "EOF\n"
+        with open(self._cmd, "w") as script_file:
+            script_file.write(script)
+        os.chmod(self._cmd, 0o755)
 
     def finish(self) -> None:
         self.finish_time = time.time()
