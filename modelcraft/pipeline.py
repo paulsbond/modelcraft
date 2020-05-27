146b6f3686fe709a2511eaa85ceabb79341b7c66
Paul Bond <git@paulsbond.co.uk>
2020-05-27 13:02:02 +0100

    Undoing prune resolution change

--- modelcraft/pipeline.py
+++ modelcraft/pipeline.py
@@ -61,7 +61,7 @@ class Pipeline:
         self.terminate(reason="Normal")
 
     def run_cycle(self):
-        if self.cycle > 1:
+        if self.cycle > 1 and self.resolution < 2.3:
             self.prune()
         self.parrot()
         if self.current_structure is not None:
