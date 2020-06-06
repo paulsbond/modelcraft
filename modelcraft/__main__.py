833d14a25874be7f4d68e6af79319b43f6bc981e
Paul Bond <git@paulsbond.co.uk>
2020-06-06 23:23:14 +0100

    raise EnvironmentError instead of sys.exit

--- modelcraft/__main__.py
+++ modelcraft/__main__.py
@@ -1,11 +1,11 @@
-from modelcraft.pipeline import Pipeline
 import os
 import sys
+from modelcraft.pipeline import Pipeline
 
 
 def main(argument_list=None):
     if "CCP4" not in os.environ:
-        sys.exit("CCP4 environment not set")
+        raise EnvironmentError("CCP4 environment not set")
     if argument_list is None:
         argument_list = sys.argv[1:]
     Pipeline(argument_list)
