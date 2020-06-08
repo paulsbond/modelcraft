5f176e3b6aa05431353f8b588ec5473215d7fcdb
Paul Bond <git@paulsbond.co.uk>
2020-06-08 23:19:20 +0100

    Fixed test name

--- tests/integration/test_sheetbend.py
+++ tests/integration/test_sheetbend.py
@@ -1,11 +1,11 @@
 import gemmi
-from modelcraft.reflections import DataItem
 from modelcraft.jobs import Sheetbend
+from modelcraft.reflections import DataItem
 from modelcraft.structure import ModelStats, read_structure
 from tests.integration import ccp4_path
 
 
-def test_1kv9():
+def test_1rxf():
     mtz_path = ccp4_path("examples", "data", "1rxf.mtz")
     mtz = gemmi.read_mtz_file(mtz_path)
     fsigf = DataItem(mtz, "F,SIGF")
