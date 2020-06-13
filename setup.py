e833a7913ed95a1cf7d5499ee5fb94435bb84570
Paul Bond <git@paulsbond.co.uk>
2020-06-13 01:28:13 +0100

    Widening python versions supported

--- setup.py
+++ setup.py
@@ -18,11 +18,12 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    python_requires=">=3.6, <=3.8",
+    python_requires=">=3.6",
     install_requires=["gemmi>=0.3.2", "pandas"],
     entry_points={"console_scripts": ["modelcraft = modelcraft.__main__:main"]},
 )
