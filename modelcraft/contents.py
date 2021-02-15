290f0730007dfda00248935926da9ea22b6a937d
Paul Bond <git@paulsbond.co.uk>
2021-02-15 16:46:35 +0000

    Updates to contents module

--- modelcraft/contents.py
+++ modelcraft/contents.py
@@ -1,12 +1,12 @@
 from enum import Enum
-from typing import Iterator, List, Optional
+from typing import Dict, Iterator, List, Optional
 import modelcraft.residues as residues
 
 
 class PolymerType(Enum):
-    PROTEIN = "protein"
-    RNA = "rna"
-    DNA = "dna"
+    PROTEIN = "PROTEIN"
+    RNA = "RNA"
+    DNA = "DNA"
 
     @classmethod
     def from_sequence(cls, sequence: str) -> "PolymerType":
@@ -30,40 +30,48 @@ class Polymer:
     def __init__(
         self,
         sequence: str,
-        label: str = "",
-        copies: int = 1,
+        start: int = 1,
+        label: Optional[str] = None,
+        copies: Optional[int] = None,
         polymer_type: Optional[PolymerType] = None,
+        modifications: Optional[List[str]] = None,
     ):
         self.sequence = sequence.upper()
+        self.start = start
         self.label = label
         self.copies = copies
         if polymer_type is None:
-            self.polymer_type = PolymerType.from_sequence(sequence)
+            self.type = PolymerType.from_sequence(self.sequence)
         else:
-            self.polymer_type = polymer_type
+            self.type = polymer_type
+        self.modifications = modifications
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, Polymer):
             return NotImplemented
-        return self.sequence == other.sequence
+        return (
+            self.sequence == other.sequence
+            and self.type == other.type
+            and self.modifications == other.modifications
+        )
 
     @classmethod
     def from_sequence_file(
         cls, path: str, polymer_type: Optional[PolymerType] = None
     ) -> Iterator["Polymer"]:
-        label = ""
+        label = None
         sequence = ""
-        with open(path) as sequence_file:
-            for line in sequence_file:
+        with open(path) as stream:
+            for line in stream:
                 if line[0] == ">":
                     if len(sequence) > 0:
                         yield Polymer(
-                            sequence=sequence, label=label, polymer_type=polymer_type,
+                            sequence=sequence, label=label, polymer_type=polymer_type
                         )
-                    label = line[1:].split()[0]
+                    label = line[1:].strip() if line[1:].strip() else None
                     sequence = ""
-                else:
-                    sequence += line.strip()
+                elif line[0] != ";":
+                    sequence += "".join(c for c in line if c.isalpha())
         if len(sequence) > 0:
             yield Polymer(sequence=sequence, label=label, polymer_type=polymer_type)
 
@@ -75,20 +83,16 @@ class Ligand:
 
 
 class AsuContents:
-    def __init__(self, path: Optional[str] = None):
+    def __init__(self):
         self.polymers: List[Polymer] = []
         self.ligands: List[Ligand] = []
-        if path is not None:
-            self.polymers.extend(Polymer.from_sequence_file(path))
 
-    def sequence_file_lines(
-        self, polymer_type: Optional[PolymerType] = None, line_length: int = 60
-    ) -> Iterator[str]:
-        for polymer in self.polymers:
-            if polymer_type is None or polymer.polymer_type == polymer_type:
-                yield f"> {polymer.label}\n"
-                for i in range(0, len(polymer.sequence), line_length):
-                    yield polymer.sequence[i : i + line_length] + "\n"
+    def add_from_sequence_file(
+        self, path: str, polymer_type: Optional[PolymerType] = None
+    ) -> "AsuContents":
+        for polymer in Polymer.from_sequence_file(path, polymer_type):
+            if polymer not in self.polymers:
+                self.polymers.append(polymer)
 
     def write_sequence_file(
         self,
@@ -96,8 +100,9 @@ class AsuContents:
         polymer_type: Optional[PolymerType] = None,
         line_length: int = 60,
     ):
-        with open(path, "w") as sequence_file:
-            for line in self.sequence_file_lines(
-                polymer_type=polymer_type, line_length=line_length
-            ):
-                sequence_file.write(line)
+        with open(path, "w") as stream:
+            for polymer in self.polymers:
+                if polymer_type is None or polymer.type == polymer_type:
+                    stream.write(f"> {polymer.label or polymer.type}\n")
+                    for i in range(0, len(polymer.sequence), line_length):
+                        stream.write(polymer.sequence[i : i + line_length] + "\n")
