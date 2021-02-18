3379a149b7982295dc35ced3c9308fe61ab59dee
Paul Bond <git@paulsbond.co.uk>
2021-02-18 12:21:45 +0000

    Changed contnts arguments

--- modelcraft/scripts/contents.py
+++ modelcraft/scripts/contents.py
@@ -8,13 +8,12 @@ def main(argument_list=None):
         argument_list = sys.argv[1:]
     parser = argparse.ArgumentParser()
     parser.add_argument("pdbid")
-    parser.add_argument("--contents_json", default="modelcraft-contents.json")
-    parser.add_argument("--protein_fasta")
-    parser.add_argument("--rna_fasta")
-    parser.add_argument("--dna_fasta")
+    parser.add_argument("--contents-json", default="modelcraft-contents.json")
+    parser.add_argument("--protein-fasta")
+    parser.add_argument("--rna-fasta")
+    parser.add_argument("--dna-fasta")
     args = parser.parse_args(argument_list)
-    contents = AsuContents()
-    contents.add_from_pdbid(args.pdbid)
+    contents = AsuContents(args.pdbid)
     contents.write_json_file(args.contents_json)
     if args.protein_fasta is not None:
         contents.write_sequence_file(args.protein_fasta, PolymerType.PROTEIN)
