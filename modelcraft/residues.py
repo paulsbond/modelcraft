8bd7ca83b2b7dd2b8b23d69e4e910b3b22f094d2
Paul Bond <git@paulsbond.co.uk>
2021-02-24 13:50:55 +0000

    Added a VdW volume calculation

--- modelcraft/residues.py
+++ modelcraft/residues.py
@@ -1,6 +1,8 @@
 from functools import lru_cache
+from math import pi
 from typing import Set
 import os
+import gemmi
 
 
 @lru_cache(maxsize=None)
@@ -18,3 +20,16 @@ def _buffer_codes() -> Set[str]:
                 code = line.split(",")[0]
                 agents.add(code)
     return agents
+
+
+@lru_cache(maxsize=None)
+def vdw_volume(name: str, include_hydrogrens: bool = False) -> float:
+    filename = name.upper() + ".cif"
+    path = os.path.join(os.environ["CLIBD"], "monomers", name[0].lower(), filename)
+    block = gemmi.cif.read(path)[-1]
+    chemcomp = gemmi.make_chemcomp_from_block(block)
+    volume = 0
+    for atom in chemcomp.atoms:
+        if include_hydrogrens or not atom.is_hydrogen():
+            volume += 4 / 3 * pi * atom.el.vdw_r ** 3
+    return volume
--- tests/integration/test_residues.py
+++ tests/integration/test_residues.py
@@ -1,6 +1,12 @@
-from modelcraft.residues import is_buffer
+from modelcraft.residues import is_buffer, vdw_volume
+from math import isclose
 
 
-def test_is_buffer(code, expected):
+def test_is_buffer():
     assert is_buffer("GOL")
     assert not is_buffer("ALA")
+
+
+def test_vdw_volume():
+    assert isclose(vdw_volume("HOH"), 14.71, abs_tol=0.005)
+    assert isclose(vdw_volume("HOH", include_hydrogrens=True), 29.19, abs_tol=0.005)
