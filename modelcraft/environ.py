1f7c7635fb8bfdee2b5dea1ba6aeb97fc9b9024e
Paul Bond <git@paulsbond.co.uk>
2021-05-19 11:31:47 +0100

    Setting LD_LIBRARY_PATH

--- modelcraft/environ.py
+++ modelcraft/environ.py
@@ -2,9 +2,10 @@ import os
 
 
 def setup_environ():
-    for variable in ("CCP4", "CLIBD_MON"):
+    for variable in ("CCP4", "CLIB", "CLIBD_MON"):
         if variable not in os.environ:
             raise EnvironmentError(variable + " environment variable not set")
+    os.environ["LD_LIBRARY_PATH"] = os.environ["CLIB"]
     os.environ["COOT_N_THREADS"] = "1"
     os.environ["GOTO_NUM_THREADS"] = "1"
     os.environ["MKL_NUM_THREADS"] = "1"
