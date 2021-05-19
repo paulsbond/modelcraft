cea10d1499e3f0bdadaf6d1dddeb402411e7d315
Paul Bond <git@paulsbond.co.uk>
2021-05-19 20:20:01 +0100

    Added more ctruncate tests

--- tests/integration/test_ctruncate.py
+++ tests/integration/test_ctruncate.py
@@ -4,10 +4,19 @@ from modelcraft.reflections import DataItem
 from tests.integration import ccp4_path
 
 
-def test_imean():
-    mtz_path = ccp4_path("examples", "data", "1rxf.mtz")
-    mtz = gemmi.read_mtz_file(mtz_path)
-    observations = DataItem(mtz, "I,SIGI")
+def fanom_test(path: str, label: str) -> None:
+    mtz = gemmi.read_mtz_file(path)
+    observations = DataItem(mtz, label)
+    ctruncate = CTruncate(observations=observations).run()
+    assert ctruncate.fmean.nreflections == observations.nreflections
+    assert ctruncate.fanom == observations
+    assert ctruncate.imean is None
+    assert ctruncate.ianom is None
+
+
+def imean_test(path: str, label: str) -> None:
+    mtz = gemmi.read_mtz_file(path)
+    observations = DataItem(mtz, label)
     ctruncate = CTruncate(observations=observations).run()
     assert ctruncate.fmean.nreflections == observations.nreflections
     assert ctruncate.fanom is None
@@ -15,12 +24,46 @@ def test_imean():
     assert ctruncate.ianom is None
 
 
-def test_ianom():
-    mtz_path = ccp4_path("examples", "data", "1vr7_lr_i.mtz")
-    mtz = gemmi.read_mtz_file(mtz_path)
-    observations = DataItem(mtz, "I(+),SIGI(+),I(-),SIGI(-)")
+def ianom_test(path: str, label: str) -> None:
+    mtz = gemmi.read_mtz_file(path)
+    observations = DataItem(mtz, label)
     ctruncate = CTruncate(observations=observations).run()
     assert ctruncate.fmean.nreflections == observations.nreflections
     assert ctruncate.fanom.nreflections == observations.nreflections
     assert ctruncate.imean.nreflections == observations.nreflections
     assert ctruncate.ianom == observations
+
+
+def test_1rxf():
+    path = ccp4_path("examples", "data", "1rxf.mtz")
+    imean_test(path, "I,SIGI")
+
+
+def test_ianom():
+    path = ccp4_path("examples", "data", "1vr7_lr_i.mtz")
+    imean_test(path, "IMEAN,SIGIMEAN")
+    ianom_test(path, "I(+),SIGI(+),I(-),SIGI(-)")
+
+
+def test_deuterolysin():
+    path = ccp4_path("examples", "data", "deuterolysin.mtz")
+    fanom_test(path, "F(+),SIGF(+),F(-),SIGF(-)")
+    imean_test(path, "IMEAN,SIGIMEAN")
+    ianom_test(path, "I(+),SIGI(+),I(-),SIGI(-)")
+
+
+def test_gere():
+    path = ccp4_path("examples", "data", "gere.mtz")
+    fanom_test(path, "F(+),SIGF(+),F(-),SIGF(-)")
+    imean_test(path, "IMEAN,SIGIMEAN")
+    ianom_test(path, "I(+),SIGI(+),I(-),SIGI(-)")
+
+
+def test_insulin():
+    path = ccp4_path("examples", "data", "insulin.mtz")
+    imean_test(path, "IMEAN,SIGIMEAN")
+
+
+def test_insulin_ssad():
+    path = ccp4_path("examples", "data", "SSADinsulin.mtz")
+    fanom_test(path, "F_CuKa(+),SIGF_CuKa(+),F_CuKa(-),SIGF_CuKa(-)")
