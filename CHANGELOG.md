7caa2107532bf1209845b5c5e30620db5765d3ce
Paul Bond <git@paulsbond.co.uk>
2021-11-15 11:13:07 +0000

    Version 2.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@
 All notable changes to this project will be documented in this file.  
 This project adheres to [Semantic Versioning](https://semver.org).
 
-## [2.0.1] - Unreleased
+## [2.0.1] - 2021-11-15
 
 ### Added
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="modelcraft",
-    version="2.0.0",
+    version="2.0.1",
     author="Paul Bond",
     author_email="paul.bond@york.ac.uk",
     description="Automated model building pipeline for X-ray crystallography",
