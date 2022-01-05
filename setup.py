cd1b0cc7bfd809912849d49b4de44d4291e6e003
Paul Bond <git@paulsbond.co.uk>
2022-01-05 10:13:24 +0000

    Using gemmi.CoorFormat.Detect

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     ],
     python_requires="~=3.7",
     install_requires=[
-        "gemmi >=0.4.8",
+        "gemmi >=0.5.1",
         "numpy",
         "pandas",
         "requests",
