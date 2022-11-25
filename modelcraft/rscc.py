284cc06518ac189bd81a6d27e5ab8a8f4e194cd4
Paul Bond <git@paulsbond.co.uk>
2022-11-25 13:05:01 +0000

    Model index argument

--- modelcraft/rscc.py
+++ modelcraft/rscc.py
@@ -4,23 +4,26 @@ from modelcraft.reflections import DataItem
 
 
 def per_residue_rscc(
-    structure: gemmi.Structure, fphi: DataItem, radius: float = 1.5
+    structure: gemmi.Structure,
+    fphi: DataItem,
+    radius: float = 1.5,
+    model_index: int = 0,
 ) -> dict:
     calculator = gemmi.DensityCalculatorX()
     calculator.d_min = fphi.resolution_high()
     calculator.set_grid_cell_and_spacegroup(structure)
-    calculator.put_model_density_on_grid(structure[0])
+    calculator.put_model_density_on_grid(structure[model_index])
     density = fphi.transform_f_phi_to_map(
         fphi.label(0), fphi.label(1), exact_size=calculator.grid.shape
     )
-    search = gemmi.NeighborSearch(structure, max_radius=radius)
+    search = gemmi.NeighborSearch(structure, radius, model_index)
     search.populate(include_h=False)
     residue_pairs = {}
     for point in density.masked_asu():
         position = density.point_to_position(point)
         mark = search.find_nearest_atom(position)
         if mark is not None:
-            cra = mark.to_cra(structure[0])
+            cra = mark.to_cra(structure[model_index])
             key = (cra.chain.name, str(cra.residue.seqid))
             value1 = point.value
             value2 = calculator.grid.get_value(point.u, point.v, point.w)
