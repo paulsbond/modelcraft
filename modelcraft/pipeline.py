f37138985f962b01ed52ceadef69a958698f7bda
Paul Bond <git@paulsbond.co.uk>
2023-03-02 16:33:59 +0000

    Reporting start and finish for all jobs

--- modelcraft/pipeline.py
+++ modelcraft/pipeline.py
@@ -1,6 +1,9 @@
 import collections
-import os
+import dataclasses
 import itertools
+import json
+import os
+import time
 
 
 class Pipeline:
@@ -12,9 +15,38 @@ class Pipeline:
         self.keep_jobs = keep_jobs
         self.keep_logs = keep_logs
         self.seconds = collections.defaultdict(float)
+        self.report = {"seconds": self.seconds, "jobs": []}
+        self.start_time = None
 
     def path(self, *paths: str) -> str:
         return os.path.join(self.directory, *paths)
 
     def next_job_directory(self, name: str) -> str:
         return self.path(f"job_{next(self._numbers)}_{name}")
+
+    def report_job_start(self, name):
+        if self.start_time is None:
+            self.start_time = time.time()
+        print(name, flush=True)
+        self.report["running_job"] = name
+        self.write_report()
+
+    def report_job_finish(self, result):
+        name = self.report.pop("running_job")
+        result_dict = {}
+        for field in dataclasses.fields(result):
+            value = getattr(result, field.name)
+            try:
+                json.dumps(value)
+            except TypeError:
+                pass
+            else:
+                result_dict[field.name] = value
+        print(json.dumps(result_dict, indent=4), flush=True)
+        self.report["jobs"].append({"name": name, **result_dict})
+        self.write_report()
+
+    def write_report(self):
+        self.seconds["total"] = time.time() - self.start_time
+        with open(self.path("modelcraft.json"), "w") as report_file:
+            json.dump(self.report, report_file, indent=4)
