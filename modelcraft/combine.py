75d9585abc18a64a42ff5f444d63701b5c167011
Paul Bond <git@paulsbond.co.uk>
2024-02-27 12:01:07 +0000

    Combine rework (in progress)

--- modelcraft/combine.py
+++ modelcraft/combine.py
@@ -3,160 +3,127 @@ import gemmi
 from .jobs.refmac import RefmacResult
 
 
-@dataclasses.dataclass(unsafe_hash=True)
-class _Clash:
-    key1: tuple
-    key2: tuple
-
-
-@dataclasses.dataclass(unsafe_hash=True)
-class _ClashZone:
-    keys1: list
-    keys2: list
-
-
 def combine_results(result1: RefmacResult, result2: RefmacResult) -> gemmi.Structure:
-    scores1 = _residue_scores(result1)
-    scores2 = _residue_scores(result2)
+    residues1 = _my_residues(result1.structure)
+    residues2 = _my_residues(result2.structure)
+    _assign_scores(residues1, _scores(result1))
+    _assign_scores(residues2, _scores(result2))
+    clashes = _clashes(result1.structure, result2.structure)
+    _assign_clashes(residues1, residues2, clashes)
 
-    structure1 = result1.structure
-    structure2 = result2.structure
-    clashes = identify_clashes(structure1, structure2)
-    clash_zones = identify_clash_zones(clashes, structure2)
+    # to_remove1 = set()
+    # to_remove2 = set()
+    # for clash_zone in clash_zones:
+    #     total_score1 = score_from_zone(clash_zone.keys1, scores1, structure1)
+    #     total_score2 = score_from_zone(clash_zone.keys2, scores2, structure2)
+    #     if total_score2 > total_score1:
+    #         for key1 in clash_zone.keys1:
+    #             to_remove1.add(key1)
+    #     else:
+    #         for key2 in clash_zone.keys2:
+    #             to_remove2.add(key2)
 
-    to_remove1 = set()
-    to_remove2 = set()
-    for clash_zone in clash_zones:
-        total_score1 = score_from_zone(clash_zone.keys1, scores1, structure1)
-        total_score2 = score_from_zone(clash_zone.keys2, scores2, structure2)
-        if total_score2 > total_score1:
-            for key1 in clash_zone.keys1:
-                to_remove1.add(key1)
-        else:
-            for key2 in clash_zone.keys2:
-                to_remove2.add(key2)
-
-    combined_structure = rebuild_model(structure1, structure2, to_remove1, to_remove2)
-    return combined_structure
+    # combined_structure = rebuild_model(structure1, structure2, to_remove1, to_remove2)
+    # return combined_structure
 
 
-def _residue_scores(result: RefmacResult) -> dict:
-    search = gemmi.NeighborSearch(result.structure, 5).populate()
-    fphi = result.fphi_diff
-    density = fphi.transform_f_phi_to_map(fphi.label(0), fphi.label(1))
-    stats = {}
-    for point in density.masked_asu():
-        position = density.point_to_position(point)
+def _key(chain: gemmi.Chain, residue: gemmi.Residue) -> tuple:
+    return (chain.name, residue.seqid.num, residue.seqid.icode)
+
+
+@dataclasses.dataclass
+class _MyResidue:
+    def __init__(self, chain: gemmi.Chain, residue: gemmi.Residue):
+        self.chain = chain
+        self.residue = residue
+        self.next = None
+        self.prev = None
+        self.score = None
+        self.clashing = set()
+        self.zone = None
+
+    def __hash__(self) -> int:
+        return hash(_key(self.chain, self.residue))
+
+
+def _my_residues(structure: gemmi.Structure) -> dict:
+    my_residues = {}
+    for chain in structure[0]:
+        previous = None
+        for residue in chain:
+            my_residue = _MyResidue(chain, residue)
+            my_residues[_key(chain, residue)] = my_residue
+            if previous is not None and _are_neighbours(previous, my_residue):
+                my_residue.prev = previous
+                previous.next = my_residue
+            previous = my_residue
+    return my_residues
+
+
+def _are_neighbours(res1: _MyResidue, res2: _MyResidue) -> bool:
+    res1 = res1.residue
+    res2 = res2.residue
+    info1 = gemmi.find_tabulated_residue(res1.name)
+    info2 = gemmi.find_tabulated_residue(res2.name)
+    return (
+        info1.is_amino_acid()
+        and info2.is_amino_acid()
+        and "C" in res1
+        and "N" in res2
+        and res1["C"][0].pos.dist(res2["N"][0].pos) < 2
+    ) or (
+        info1.is_nucleic_acid()
+        and info2.is_nucleic_acid()
+        and "O3'" in res1
+        and "P" in res2
+        and res1["O3'"][0].pos.dist(res2["P"][0].pos) < 2
+    )
+
+
+def _scores(result: RefmacResult) -> dict:
+    search = gemmi.NeighborSearch(result.structure, max_radius=5).populate()
+    diff_density = result.fphi_diff.transform_f_phi_to_map(
+        result.fphi_diff.label(0),
+        result.fphi_diff.label(1),
+        sample_rate=(result.fphi_diff.resolution_high() / 0.7),  # 0.7A grid spacing
+    )
+    sum_counts = {}
+    for point in diff_density.masked_asu():
+        position = diff_density.point_to_position(point)
         mark = search.find_nearest_atom(position)
         if mark is not None:
             cra = mark.to_cra(result.structure[0])
-            key = (cra.chain.name, str(cra.residue.seqid))
-            stats.setdefault(key, [0, 0])
+            key = _key(cra.chain, cra.residue)
+            sum_counts.setdefault(key, [0, 0])
             if point.value > 0:
-                stats[key][0] += point.value
-            stats[key][1] += 1
-    return {k: -v[0] / v[1] if v[1] > 0 else 0 for k, v in stats.items()}
+                sum_counts[key][0] += point.value
+            sum_counts[key][1] += 1
+    return {key: -sum_count[0] / sum_count[1] for key, sum_count in sum_counts.items()}
 
 
-def identify_clashes(structure1: gemmi.Structure, structure2: gemmi.Structure) -> set:
-    search = gemmi.NeighborSearch(structure2[0], structure2.cell, 1).populate()
+def _assign_scores(residues: dict, scores: dict) -> None:
+    for key, score in scores.items():
+        residues[key].score = score
+
+
+def _clashes(structure1: gemmi.Structure, structure2: gemmi.Structure) -> set:
+    search = gemmi.NeighborSearch(structure2, max_radius=1).populate()
     clashes = set()
     for chain1 in structure1[0]:
         for residue1 in chain1:
+            key1 = _key(chain1, residue1)
             for atom1 in residue1:
-                for atom2 in search.find_atoms(atom1.pos, alt="\0", radius=1):
-                    chain2 = structure2[0][atom2.chain_idx]
-                    residue2 = chain2[atom2.residue_idx]
-                    if gemmi.find_tabulated_residue(residue2.name).is_nucleic_acid():
-                        clash = _Clash(
-                            key1=(chain1.name, str(residue1.seqid)),
-                            key2=(chain2.name, str(residue2.seqid)),
-                        )
-                        clashes.add(clash)
+                for mark in search.find_atoms(atom1.pos, radius=1):
+                    cra = mark.to_cra(structure2[0])
+                    key2 = _key(cra.chain, cra.residue)
+                    clashes.add((key1, key2))
     return clashes
 
 
-def identify_clash_zones(clashes: set, structure2: gemmi.Structure):
-    clash_map = {}
-    for clash in clashes:
-        clash_map.setdefault(clash.key2, []).append(clash.key1)
-
-    zones2 = []
-    zone2 = []
-    previous_key2 = None
-    for key2 in sorted(list(clash_map)):
-        if previous_key2 is None or is_sequential(structure2, previous_key2, key2):
-            zone2.append(key2)
-        else:
-            zones2.append(zone2)
-            zone2 = [key2]
-        previous_key2 = key2
-    zones2.append(zone2)
-
-    zones = []
-    for zone2 in zones2:
-        zone1 = set()
-        for key2 in zone2:
-            for key1 in clash_map.get(key2):
-                zone1.add(key1)
-        zone1 = sorted(zone1)
-        zone = _ClashZone(keys1=zone1, keys2=zone2)
-        zones.append(zone)
-    return zones
-
-
-def is_sequential(structure: gemmi.Structure, key1: tuple, key2: tuple) -> bool:
-    key1_residue = extract_residue(key1, structure)
-    key2_residue = extract_residue(key2, structure)
-    key1_o3 = key1_residue.sole_atom("O3'").pos
-    key1_p = key1_residue.sole_atom("P").pos
-    key2_o3 = key2_residue.sole_atom("O3'").pos
-    key2_p = key2_residue.sole_atom("P").pos
-    return key1_o3.dist(key2_p) < 2 or key2_o3.dist(key1_p) < 2
-
-
-def extract_residue(clash_key: tuple, structure: gemmi.Structure) -> gemmi.Residue:
-    chain_name = clash_key[0]
-    residue_seqid = clash_key[1]
-    chain = structure[0].find_chain(chain_name)
-    residue_group = chain[residue_seqid]
-    return residue_group[0]
-
-
-def score_from_zone(zone: list, scores: dict, structure: gemmi.Structure) -> float:
-    score_sum = 0
-
-    for index, key in enumerate(zone):
-        chain_name, residue_seq_id = key
-        chain = structure[0].find_chain(chain_name)
-        residue = chain[residue_seq_id][0]
-
-        score = scores.get(key, 0)
-        count = 1
-
-        if index == 0:
-            previous_residue = chain.previous_residue(residue)
-            if not previous_residue:
-                continue
-
-            previous_residue_key = (chain_name, str(previous_residue.seqid))
-            if previous_residue_key in scores:
-                score += scores.get(previous_residue_key, 0)
-                count += 1
-
-        if index == len(zone) - 1:
-            next_residue = chain.next_residue(residue)
-            if not next_residue:
-                continue
-
-            next_residue_key = (chain_name, str(next_residue.seqid))
-            if next_residue_key in scores:
-                score += scores.get(next_residue_key, 0)
-                count += 1
-
-        score_sum += score / count
-
-    return score_sum / len(zone) if zone else 0
+def _assign_clashes(residues1: dict, residues2: dict, clashes: set) -> None:
+    for key1, key2 in clashes:
+        residues1[key1].clashing.add(residues2[key2])
+        residues2[key2].clashing.add(residues1[key1])
 
 
 def rebuild_model(
