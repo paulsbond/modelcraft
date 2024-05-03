5ce98fc053e5d504bf961bad58251a74e9603914
Paul Bond <git@paulsbond.co.uk>
2024-05-03 15:46:42 +0100

    Updated RSZD

--- modelcraft/rszd.py
+++ modelcraft/rszd.py
@@ -6,16 +6,19 @@ https://doi.org/10.1107/S0907444911035918
 """
 
 import gemmi
+import scipy.stats
 from .reflections import DataItem
 
 
-def rszd(structure: gemmi.Structure, fphi_diff: DataItem, model_index: int = 0) -> dict:
+def per_residue_rszd(
+    structure: gemmi.Structure, fphi_diff: DataItem, model_index: int = 0
+) -> dict:
     dmin = fphi_diff.resolution_high()
     radii = {}
     for ci, chain in enumerate(structure[model_index]):
         for ri, residue in enumerate(chain):
             for ai, atom in enumerate(residue):
-                radii[(ci, ri, ai)] = max_radius(dmin, atom)
+                radii[(ci, ri, ai, atom.altloc)] = max_radius(dmin, atom)
 
     search_radius = max(radii.values())
     search = gemmi.NeighborSearch(structure, search_radius, model_index)
@@ -25,16 +28,27 @@ def rszd(structure: gemmi.Structure, fphi_diff: DataItem, model_index: int = 0) 
     diff_den.normalize()  # TODO: Estimation of the standard uncertainty in Δρ
 
     # TODO: Calculate RSZD for main chain and side chain separately
-    residue_values = {}
+    values_dict = {}
     for point in diff_den.masked_asu():
         position = diff_den.point_to_position(point)
-        mark = search.find_nearest_atom(position)
+        mark = search.find_nearest_atom(position, search_radius)
         if mark is not None:
-            distance = mark.pos.dist(position)  # TODO: Handle image_idx
-            if distance < radii[(mark.chain_idx, mark.residue_idx, mark.atom_idx)]:
+            nearest = structure.cell.find_nearest_pbc_image(position, mark.pos, 0)
+            key = (mark.chain_idx, mark.residue_idx, mark.atom_idx, mark.altloc)
+            if nearest.dist() < radii[key]:
                 cra = mark.to_cra(structure[model_index])
                 key = (cra.chain.name, str(cra.residue.seqid))
-                residue_values.setdefault(key, []).append(point.value)
+                values_dict.setdefault(key, []).append(point.value)
+
+    rszd_dict = {}
+    for key, values in values_dict.items():
+        # TODO: Statistically independent difference density values from resampling
+        s = sum(x * x for x in values)
+        n = len(values)
+        p = 1 - scipy.stats.chi2.cdf(s, n)
+        z = abs(scipy.stats.norm.ppf(p / 2))
+        rszd_dict[key] = z
+    return rszd_dict
 
 
 def max_radius(dmin: float, atom: gemmi.Atom):
