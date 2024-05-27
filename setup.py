543551871dbe75b610048c5142cc1ba88f3371bb
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-05-27 16:58:31 +0100

    Update setup.py and add post-install command
    This commit introduces a post-install command in setup.py and increases the version from 3.6.0 to 5.0.0. It also adds nucleofind to the requirements and the installation dependencies of the package.

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import setuptools
+from setuptools.command.install import install
+import os
 
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
 
+
+class InstallPackage(install):
+    def __init__(self, dist):
+        super(install, self).__init__(dist)
+
+    def run(self):
+        install.run(self)
+        self.__post_install()
+
+    def __post_install(self):
+        os.system('nucleofind-install --all')
+
+
 setuptools.setup(
     name="modelcraft",
-    version="3.6.0",
+    version="5.0.0",
     author="Paul Bond",
     author_email="paul.bond@york.ac.uk",
     description="Automated model building pipeline for X-ray crystallography",
@@ -31,6 +46,7 @@ setuptools.setup(
         "pandas",
         "requests",
         "scipy",
+        "nucleofind"
     ],
     entry_points={
         "console_scripts": [
@@ -39,4 +55,5 @@ setuptools.setup(
             "modelcraft-copies = modelcraft.scripts.copies:main",
         ]
     },
+    cmdclass={'install': InstallPackage}
 )
