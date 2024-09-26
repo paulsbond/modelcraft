6f1eb0520265a2341fbbe81d6b6618485db00979
Paul Bond <git@paulsbond.co.uk>
2024-09-26 11:05:51 +0100

    Removed RSZD

--- modelcraft/tests/ccp4/test_rszd.py
+++
@@ -1,9 +0,0 @@
-from modelcraft.rszd import per_residue_rszd
-from . import insulin_refmac
-
-
-def test_insulin():
-    refmac = insulin_refmac()
-    rszd = per_residue_rszd(refmac.structure, refmac.fphi_diff)
-    assert len(rszd) > 0
-    assert all(len(k) == 2 and v >= 0 for k, v in rszd.items())
