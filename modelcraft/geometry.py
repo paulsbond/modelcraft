f1583122443609ca2c7bede1db29e234d48e3e08
Paul Bond <git@paulsbond.co.uk>
2024-11-12 22:14:18 +0000

    Replaced old geometry function

--- modelcraft/geometry.py
+++ modelcraft/geometry.py
@@ -1,32 +1,57 @@
-import math
+import collections
 import os
 import gemmi
-from modelcraft.monlib import in_library
+import numpy as np
 
 
-def rmsz(structure: gemmi.Structure) -> float:
-    codes = [code for code in structure[0].get_all_residue_names() if in_library(code)]
-    monlib = gemmi.read_monomer_lib(os.environ["CLIBD_MON"], codes)
-    devnull = open(os.devnull, "w")
-    topology = gemmi.prepare_topology(structure, monlib, warnings=devnull)
-    num_of_squares = 0
-    sum_of_squares = 0.0
-    for bond in topology.bonds:
-        num_of_squares += 1
-        sum_of_squares += bond.calculate_z() ** 2
-    for angle in topology.angles:
-        num_of_squares += 1
-        sum_of_squares += angle.calculate_z() ** 2
-    for torsion in topology.torsions:
+def topology(
+    structure: gemmi.Structure, model_index: int = 0, libin: str = ""
+) -> gemmi.Topo:
+    monlib = gemmi.read_monomer_lib(
+        monomer_dir=os.environ["CLIBD_MON"],
+        resnames=structure[model_index].get_all_residue_names(),
+        libin=libin or "",
+    )
+    return gemmi.prepare_topology(
+        st=structure,
+        monlib=monlib,
+        model_index=model_index,
+    )
+
+
+def per_residue_geometry_rmsz(
+    structure: gemmi.Structure, model_index: int = 0, libin: str = ""
+) -> dict:
+    atom_zs = _atom_zs(structure, model_index, libin)
+    rv = {}
+    for chain in structure[model_index]:
+        for residue in chain:
+            zs = np.concatenate([atom_zs.get(atom.serial, []) for atom in residue])
+            rmsz = np.sqrt(np.mean(np.square(zs))) if len(zs) > 0 else np.nan
+            rv[(chain.name, str(residue.seqid))] = rmsz
+    return rv
+
+
+def _atom_zs(structure: gemmi.Structure, model_index, libin) -> dict:
+    structure.assign_serial_numbers()
+    topo = topology(structure, model_index, libin)
+    atom_zs = collections.defaultdict(list)
+    for bond in topo.bonds:
+        z = bond.calculate_z()
+        for atom in bond.atoms:
+            atom_zs[atom.serial].append(z)
+    for angle in topo.angles:
+        z = angle.calculate_z()
+        for atom in angle.atoms:
+            atom_zs[atom.serial].append(z)
+    for torsion in topo.torsions:
         if torsion.restr.esd > 0:
-            num_of_squares += 1
-            sum_of_squares += torsion.calculate_z() ** 2
-    for plane in topology.planes:
+            z = torsion.calculate_z()
+            for atom in torsion.atoms:
+                atom_zs[atom.serial].append(z)
+    for plane in topo.planes:
         best_plane = gemmi.find_best_plane(plane.atoms)
-        max_z = 0
         for atom in plane.atoms:
-            distance = gemmi.get_distance_from_plane(atom.pos, best_plane)
-            max_z = max(distance / plane.restr.esd, max_z)
-        num_of_squares += 1
-        sum_of_squares += max_z ** 2
-    return math.sqrt(sum_of_squares / num_of_squares)
+            z = gemmi.get_distance_from_plane(atom.pos, best_plane) / plane.restr.esd
+            atom_zs[atom.serial].append(z)
+    return atom_zs
--- modelcraft/validation.py
+++ modelcraft/validation.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from os import environ
 import gemmi
 import numpy as np
 import pandas as pd
@@ -101,36 +100,3 @@ def _rscc_diff(
         rscc[key] = np.corrcoef(best_value, calc_values[key])[0, 1]
         diff[key] = np.sqrt(np.mean(np.square(diff_values[key])))
     return rscc, diff
-
-
-def _geom(structure: gemmi.Structure, model_index: int, libin: str) -> dict:
-    structure.assign_serial_numbers()
-    resnames = structure[model_index].get_all_residue_names()
-    monlib = gemmi.read_monomer_lib(environ["CLIBD_MON"], resnames, libin)
-    topo = gemmi.prepare_topology(structure, monlib, model_index)
-    atom_zs = defaultdict(list)
-    for bond in topo.bonds:
-        z = bond.calculate_z()
-        for atom in bond.atoms:
-            atom_zs[atom.serial].append(z)
-    for angle in topo.angles:
-        z = angle.calculate_z()
-        for atom in angle.atoms:
-            atom_zs[atom.serial].append(z)
-    for torsion in topo.torsions:
-        if torsion.restr.esd > 0:
-            z = torsion.calculate_z()
-            for atom in torsion.atoms:
-                atom_zs[atom.serial].append(z)
-    for plane in topo.planes:
-        best_plane = gemmi.find_best_plane(plane.atoms)
-        for atom in plane.atoms:
-            z = gemmi.get_distance_from_plane(atom.pos, best_plane) / plane.restr.esd
-            atom_zs[atom.serial].append(z)
-    geom = {}
-    for chain in structure[model_index]:
-        for residue in chain:
-            zs = np.concatenate([atom_zs.get(atom.serial, []) for atom in residue])
-            rmsz = np.sqrt(np.mean(np.square(zs))) if len(zs) > 0 else np.nan
-            geom[(chain.name, str(residue.seqid))] = rmsz
-    return geom
