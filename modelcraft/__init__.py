6d15fd07156055465ab394e8533287519744f224
Paul Bond <git@paulsbond.co.uk>
2025-10-13 22:19:24 +0100

    Free-R flag value handling

--- modelcraft/__init__.py
+++ modelcraft/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.0.4"
+__version__ = "5.1.0"
 
 from .cell import max_distortion as max_cell_distortion
 from .cell import remove_scale
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="modelcraft",
-    version="5.0.4",
+    version="5.1.0",
     author="Paul Bond",
     author_email="paul.bond@york.ac.uk",
     description="Automated model building pipeline for X-ray crystallography",
